d44c626ed66750b5b3389d27d439b0219184a537
nonineeeee <vhubji0331@gmail.com>
2026-09-24 11:36:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyQ-d9MLAsQkPs7LUS6RE-6_2Jio8NcENCTbyvxqm55uRlvYBmlbIuZPWMEH0GHzfve/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzBgtnbJQnIJBHuxkQEruFCewgTAThkuye9y-JDktEgwxKQA-XjPsqAgvTGiaIQjkJl/exec";
 const API_TIMEOUT_MS = 25000;
